efa7994f1003b80df38001d244978021d0d3ddc9
CodeKrakken <newholm1@hotmail.com>
2021-02-09 13:10:53 +0000

    Test for length of letter values hash

--- test/test.js
+++ test/test.js
@@ -1,5 +1,22 @@
 const { pairingTest } = require('../src/pairing-test');
+const Game = require('../src/Game')
 
-test('a failing test', () => {
-    expect(pairingTest()).toBe(true);
+let game
+
+test('a once failing test, now passing', () => {
+  expect(pairingTest()).toBe(true);
 });
+
+describe('game', function() {
+  beforeEach(function() {
+    game = new Game
+  })
+
+  it('has a hash of letter values', function() {
+    expect(game.letterValues).toBeDefined()
+  })
+
+  it('has letter values defined in its hash', function() {
+    expect(game.size(game.letterValues)).toEqual(26)
+  })
+})
